48baab02f3d283af5880e77accfb29574867d01a
Prashant rawal <Prashantrawal619@gmail.com>
2019-04-19 13:52:28 +0530

    added test on models

---
+++ exercises/models/crud.js
@@ -0,0 +1,32 @@
+const User = require("./user");
+
+const getUserById = id => {
+  return User.findById(id).exec();
+};
+
+const getAllUsers = () => {
+  return User.find({}).exec();
+};
+
+const createUser = userDetails => {
+  return User.create({ userDetails });
+};
+const removeUserById = id => {
+  return User.findByIdAndRemove(id);
+};
+
+const updateUserById = (id, update) => {
+  return User.findByIdAndUpdate(id, update, { new: true });
+};
+
+module.exports = {
+  getUserById,
+  getAllUsers,
+  createUser,
+  removeUserById,
+  updateUserById
+};
+
+//some of my test is not working and i dont know
+// exec() function return the actual promise and it tells mongoose that i done with the query and i dont have any query after that
+//findByIdAndUpdate has the 3rd argument options here im using new:true that means its it return the newly updated object instead of old one 
